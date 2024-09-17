645b33720e8b353b96205d74f922b553ffec8671
ervin arviandi <muhamadervin34@gmail.com>
2024-09-17 14:08:51 +0700

    update card studio

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
           <p className='lg:text-lg text-sm mt-5 dark:text-[#777777]'>find me by looking at the location map below.</p>
           <hr className='border-2 border-dashed w-full mt-5' />
           <div >
-          <Link href="/" className='flex items-center gap-2 my-5 hover:text-sky-500 text-[#] '> <HiOutlineArrowCircleLeft size={20}/>Back</Link>
+          <Link href="/" className='flex items-center gap-2 my-5 hover:text-[#7bf080] text-[#] '> <HiOutlineArrowCircleLeft size={20}/>Back</Link>
           </div>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.8395613665245!2d118.43039520500704!3d-8.554082543056339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dca6f73d9d4c0a5%3A0xd3c86b70715905e7!2sCCWJ%2B9M8%2C%20Kandai%20Dua%2C%20Woja%2C%20Dompu%20Regency%2C%20West%20Nusa%20Tenggara%2C%20Indonesia!5e0!3m2!1sen!2sus!4v1725705966226!5m2!1sen!2sus"  style={{border:0}} className='h-screen w-full my-7' loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           {/* <Maps/> */}
--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -20,20 +20,20 @@ const Page = () => {
                 <p className='lg:text-lg text-sm mt-5 dark:text-[#777777]'>A work-from-home workspace that allows me to efficiently complete most tasks.</p>
                 <hr className='border-2 border-dashed w-full mt-5' />
                 <CardContainer className="inter-var">
-           <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-        <CardItem
+           <CardBody className="bg-white relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-[#121212] dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        {/* <CardItem
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
           Make things float in air
-        </CardItem>
-        <CardItem
+        </CardItem> */}
+        {/* <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
           Hover over this card to unleash the power of CSS perspective
-        </CardItem>
+        </CardItem> */}
         <CardItem
           translateZ="100"
           rotateX={20}
@@ -49,7 +49,7 @@ const Page = () => {
                 })`}
               onLoad={() => setLoading(false)} alt='MyWorkspace'/>
         </CardItem>
-        <div className="flex justify-between items-center mt-20">
+        {/* <div className="flex justify-between items-center mt-20">
           <CardItem
             translateZ={20}
             translateX={-40}
@@ -66,7 +66,7 @@ const Page = () => {
           >
             Sign up
           </CardItem>
-        </div>
+        </div> */}
       </CardBody>
     </CardContainer>
 
